e4ec4ad3e8611d81f1e5c5ecb86c4a2162c27805
Alan Unruh <alan.unruh@gmail.com>
2017-03-31 13:37:22 +0000

    Edit error text

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,7 +6,7 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
-      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid'
+      flash[:error] = '<strong>Could not save</strong> the information you entered is either too long or short.'
     end
     redirect_to root_path
   end
